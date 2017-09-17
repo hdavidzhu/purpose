6c31e4ea327dce57ba90a2940074e8ce5074e1c4
David Zhu <hdavidzhu@gmail.com>
2017-09-17 11:09:24 -0700

    Added a config raven script.

--- background/index.js
+++ background/index.js
@@ -1,8 +1,1 @@
-Raven
-  .config('https://0e9bdfe64d284a4a91f284636a538703@sentry.io/212625')
-  .install();
-
- // Wraps in try/catch
-Raven.context(function() {
-  const purposeApp = new PurposeApp();
-});
+const purposeApp = new PurposeApp();
---
+++ base/config_raven.js
@@ -0,0 +1,3 @@
+Raven
+  .config('https://0e9bdfe64d284a4a91f284636a538703@sentry.io/212625')
+  .install();
