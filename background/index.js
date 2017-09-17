311799eefbe2558aaaf492a04edee2f9ce7987e7
David Zhu <hdavidzhu@gmail.com>
2017-09-17 10:59:56 -0700

    Included instrumentation.

--- background/index.js
+++ background/index.js
@@ -1,4 +1,8 @@
+Raven
+  .config('https://0e9bdfe64d284a4a91f284636a538703@sentry.io/212625')
+  .install();
+
  // Wraps in try/catch
-Raven.context(function () {
+Raven.context(function() {
   const purposeApp = new PurposeApp();
 });
