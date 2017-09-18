8913da0c920b6ec743268b90b8b6590f1749675d
David Zhu <hdavidzhu@gmail.com>
2017-09-17 19:10:11 -0700

    Added a basic release script.

--- release.sh
+++ release.sh
@@ -7,12 +7,10 @@ echo
 echo "Enter new version:"
 read VERSION
 
-echo $VERSION
-
 # # Pull, create a version, and push.
 git checkout master
 git pull
-git tag -a $VERSION -m `${VERSION}`
+git tag -a "$VERSION" -m "${VERSION}"
 git push -u origin --tags
 
 echo "Pushed! Please visit https://github.com/hdavidzhu/purpose/releases"
